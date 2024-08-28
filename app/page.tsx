2413fe364eae5f5c7570ac1b622898acff761bff
stopnoanime <birecki@proton.me>
2024-08-28 22:38:37 +0200

    Skip next-auth signIn page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
+import { auth, signIn } from "@/auth";
 import Link from "next/link";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
   return (
     <main className="min-h-screen grid place-items-center p-8">
       <h1 className="text-4xl"> Home Page </h1>
-      <Link className="link text-lg" href="/dashboard">
-        {" "}
-        Go to dashboard{" "}
-      </Link>
+      {!!session && (
+        <Link className="link text-lg" href="/dashboard">
+          {" "}
+          Go to dashboard{" "}
+        </Link>
+      )}
+      {!session && (
+        <form
+          action={async () => {
+            "use server";
+            await signIn("github", { redirectTo: "/dashboard" });
+          }}
+        >
+          <button className="link text-lg">Sign in</button>
+        </form>
+      )}
     </main>
   );
 }
--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import github from "next-auth/providers/github";
+import { NextResponse } from "next/server";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [github],
@@ -7,13 +8,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     authorized: async ({ auth, request }) => {
       const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
 
-      if (isOnDashboard) return !!auth;
+      if (isOnDashboard && !auth)
+        return NextResponse.redirect(new URL("/", request.url));
 
       return true;
     },
     jwt({ token, account }) {
-      if (account) 
-        token.id = account.providerAccountId;
+      if (account) token.id = account.providerAccountId;
 
       return token;
     },
@@ -21,5 +22,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       session.user.id = token.id as string;
       return session;
     },
+  },
+  theme: {
+    colorScheme: "light",
   },
 });
