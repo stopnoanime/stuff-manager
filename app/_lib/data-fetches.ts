379a354c7ebd68d4e61fa964b06ec9ad85c6d555
stopnoanime <birecki@proton.me>
2024-08-28 12:31:34 +0200

    Add "server-only" to some files

--- app/_lib/data-fetches.ts
+++ app/_lib/data-fetches.ts
@@ -1,3 +1,4 @@
+import "server-only";
 import { sql } from "@vercel/postgres";
 import {
   DashboardStatistics,
--- app/_lib/data-seed.ts
+++ app/_lib/data-seed.ts
@@ -1,3 +1,4 @@
+import "server-only";
 import { sql } from "@vercel/postgres";
 
 export async function seedItems() {
