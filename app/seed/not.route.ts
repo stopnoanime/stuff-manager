91a00cfaab8a6dd568e9f687f33fdbc7b4112167
stopnoanime <birecki@proton.me>
2024-09-12 18:21:12 +0200

    Properly comment route.ts

---
+++ app/seed/not.route.ts
@@ -0,0 +1,18 @@
+import { seedCategories, seedItems } from "@/app/_lib/data-seed";
+import { sql } from "@vercel/postgres";
+
+export async function GET() {
+  try {
+    await sql`BEGIN`;
+    await seedItems();
+    await seedCategories();
+    await sql`COMMIT`;
+
+    return Response.json({
+      message: `Database seeded successfully. You can delete 'app/seed/route.ts' now.`,
+    });
+  } catch (error) {
+    await sql`ROLLBACK`;
+    return Response.json({ error }, { status: 500 });
+  }
+}
